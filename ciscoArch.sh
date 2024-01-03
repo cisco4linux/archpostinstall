51ad46dddd9ff9cc69ab518a1b25f41c6484a7c1
cisco4linux <155055486+cisco4linux@users.noreply.github.com>
2024-01-03 00:03:14 +0000

    Update ciscoArch.sh

--- ciscoArch.sh
+++ ciscoArch.sh
@@ -14,7 +14,7 @@ yes | sudo pacman -Syu
 echo "------------------------------------------"
 echo ">     Installing programs via pacman <"
 echo "------------------------------------------"
-yes | sudo pacman -Sy git firefox steam neofetch discord nvidia-driver nvidia-lts vlc bitwarden rofi tldr gimp gnome-browser-connector pacman-contrib vte3 plank openssh rsync openrgb flatpak yuzu flycast
+yes | sudo pacman -Sy git firefox steam neofetch discord nvidia-driver nvidia-lts vlc bitwarden rofi tldr gimp gnome-browser-connector pacman-contrib vte3 plank openssh rsync openrgb flatpak yuzu
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 echo "------------------------------------------"
